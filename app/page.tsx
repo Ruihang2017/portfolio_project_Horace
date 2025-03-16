63707206d05ce88629534967dbc8a2defbb1255e
Ruihang2017 <94610820+Ruihang2017@users.noreply.github.com>
2025-03-16 18:09:15 +1100

    Init

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+import PortfolioV2 from "../portfolio-v2"
+import Portfolio from "../portfolio"
+
+export default function SyntheticV0PageForDeployment() {
+  return <PortfolioV2 />
+  // return <Portfolio />
+}
